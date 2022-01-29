ffad65f3b9cc761dc81f28c2ad1e7bb123232303
mmal1nka <86218993+mmal1nka@users.noreply.github.com>
2022-01-29 15:00:23 +0700

    Update views.py

--- coursework/Vigenere/views.py
+++ coursework/Vigenere/views.py
@@ -38,24 +38,83 @@ def EncryptedTemplate(request):
 
 
 def AuthEncMes(request):
-    form = CipherForm(request.POST)
-    if request.method == "POST" and form.is_valid():
+    form = CipherForm(request.POST, request.FILES or None)
+    if request.method == "POST":
         username = request.POST.get("Log")
         password = request.POST.get("pass")
+        mes = request.POST.get('mes')
+        key = request.POST.get('key')
         try:
             checkUserLogin = Person.objects.get(Login=username, Password=password)
-            if checkUserLogin is not None:
-                mess = form.cleaned_data['Mess']
-                key = form.cleaned_data['Key']
-                mapped_key = msg_and_key(mess, key)                
-                if mapped_key == 'Error':
-                    messages.error(request, 'Incorrect message or key')
-                    return HttpResponseRedirect('encrypt')
-                else:
-                    EncMes = cipher_encryption(mess, mapped_key)
-                    crt = UserAndMessage.objects.create(EncryptMessage=EncMes, Mess=mess, UserId=checkUserLogin.id)
-                    messages.success(request, "Your encrypted message: " + EncMes)
-                    return HttpResponseRedirect('encrypt')
+            rb = request.POST.get("RB", None)
+            if rb in ["Decrypt", "Encrypt"]:
+                if rb == "Encrypt":
+                    try:
+                        ChosenFile = request.FILES['chosenFile']
+                        chosenFile = ChosenFile.name
+                        path = os.path.abspath(chosenFile)
+                        chsFile = open(path, 'a')
+                        myfile = File(chsFile)
+                        if checkUserLogin is not None:
+                            msg = mes
+                            key = key
+                            EncMes = encrypt(msg, key)
+                            if EncMes == '':
+                                data = {
+                                    'username': username,
+                                    'password': password,
+                                    'mes': mes,
+                                    'key': key,
+                                    'result': EncMes
+                                }
+                                messages.error(request, 'Incorrect message or key')
+                                return render(request, "Vigenere/encrypter.html", data)
+                            else:
+                                data = {
+                                    'username': username,
+                                    'password': password,
+                                    'mes': mes,
+                                    'key': key,
+                                    'result': EncMes
+                                }
+                                tmp = UserAndMessage.objects.create(EncryptMessage=EncMes, UserId=checkUserLogin.id)
+                                myfile.write("\nYour encrypted message: " + EncMes)
+                                myfile.closed
+                                return render(request, "Vigenere/encrypter.html", data)
+                    except MultiValueDictKeyError:
+                        data = {
+                            'username': username,
+                            'password': password,
+                            'mes': mes,
+                            'key': key,
+                        }
+                        messages.error(request, 'You have not chosen file')
+                        return render(request, "Vigenere/encrypter.html", data)
+                elif rb == "Decrypt":
+                    if checkUserLogin is not None:
+                        msg = mes
+                        key = key
+                    try:
+                        tmp = UserAndMessage.objects.filter(EncryptMessage=msg, UserId=checkUserLogin.id)
+                        if tmp is not None:
+                            EncMes = decrypt(msg, key)
+                            data = {
+                                'username': username,
+                                'password': password,
+                                'mes': mes,
+                                'key': key,
+                                'result': EncMes
+                            }
+                            return render(request, "Vigenere/encrypter.html", data)
+                    except UserAndMessage.DoesNotExist:
+                        data = {
+                            'username': username,
+                            'password': password,
+                            'mes': mes,
+                            'key': key,
+                        }
+                        messages.error(request, "Message is not found")
+                        return render(request, "Vigenere/encrypter.html", data)
         except Person.DoesNotExist:
             messages.error(request, "Wrong login or password")
             return HttpResponseRedirect('encrypt')
